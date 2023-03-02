9f9f1ea941e4a226d1652b01a2a6ba296cf72595
SeongJin Jeong <seongjin.jeong@playlinks.com>
2023-03-02 18:57:18 +0900

    * Server - Code Reformatted

--- src/Game/GameRoom.ts
+++ src/Game/GameRoom.ts
@@ -6,14 +6,12 @@ import {PacketGameInfo} from "../netHandler";
 class GameRoom extends Room {
     private manager: GameManager = null;
 
-    public initGameRoom(players: Player[], data : PacketGameInfo): void {
+    public initGameRoom(players: Player[], data: PacketGameInfo): void {
         this.players = players;
         this.manager = new GameManager(this, this.players, data);
     }
-    public startGame() : void {
-        this.manager.onGameStart();
-    }
-    public getManager() : GameManager {
+
+    public getManager(): GameManager {
         return this.manager;
     }
 }
